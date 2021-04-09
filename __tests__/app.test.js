cf1b27e979d18e412d8c2a47a9b3f23d77124ba5
Colin Bassett <ccwbassett@gmail.com>
2021-04-09 11:35:08 -0700

    added empty models and updated empty test for sql.sync

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,10 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const database = require('../sql/sequelize');
 
 describe('team-sequelize-lab routes', () => {
   beforeEach(() => {
-    return setup(pool);
+    return database.sync({ force: true})
   });
+  
 });
